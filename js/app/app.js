2f359c26cb369511299f0b2e61834b23d2c4ad45
Jesper Tullberg <jt222dg@student.lnu.se>
2013-11-15 21:04:46 -0500

    basic layout and style with bootstrap

--- js/app/app.js
+++ js/app/app.js
@@ -1,3 +1,5 @@
+console.log("SYSTEM: App module loading...");
+
 define(['backbone', 'jquery', 'underscore', 'app/view/testview', 'app/model/TestModel', 'bootstrap'],
 function(Backbone, $, _, TestView, TestModel) {
   return {
@@ -6,15 +8,21 @@ function(Backbone, $, _, TestView, TestModel) {
       var testModel2 = new TestModel();
       var testView = new TestView();
       
-      $("#button").click(function() {
-        $("#button2").show();
-        $(this).hide();
+      var button = $("#button");
+      var change = true;
+      button.click(function() {
+        button.empty();
+        button.append((change ? "Another " : "A ") + "button");
+        change = !change;
       });
       
-      $("#button2").click(function() {
-        $("#button").show();
-        $(this).hide();
+      _.each($("#menu").children(), function(link) {
+        $(link).click(function() {
+          $(".active").removeClass("active");
+          $(link).addClass("active");
+        })
       });
+      
       console.log("SYSTEM: Main application running...");
     }
   };
